8c7009c01482601bf543457fa638d52406f75b11
tero <ishigamisenku8888@gmail.com>
2025-06-10 23:34:05 +0800

    add modules to roldan and main

---
+++ main.py
@@ -0,0 +1,40 @@
+
+from mamba.buenacifra import Buenacifra, clear_screen, show_border
+from mamba.roldan import Roldan
+
+
+def menu():
+    user_input = 0
+    while user_input != 5:
+        user_input = display_get_user_input()
+        process_user_input(user_input)
+
+def display_get_user_input():
+    while True:
+        clear_screen()
+        show_border()
+        print("\t\t\t\t\tMamba Main Menu")
+        show_border()
+        print("\n1. Ernesto P. Bernas III\n")
+        print("2. Abrianne V. Buenacifra\n")
+        print("3. Gian Rafael B. Roldan\n")
+        print("4. Altheno Mari L. Tero\n")
+        print("5. Exit\n")
+        return int(input("Please select an option (1 - 5): "))
+
+def process_user_input(user_input):
+    match user_input:
+        case 1:
+            
+        case 2:
+            Buenacifra().show_menu()
+        case 3:
+            Roldan().show_menu()
+        case 4:
+            
+        case 5:
+            pass
+        case _:
+            input("\nWrong input. Press Enter to try again.")
+
+menu()
---
+++ mamba/roldan.py
@@ -0,0 +1,128 @@
+import os
+
+def show_border():
+    print(
+        "============================================================"
+        "===================================="  
+    )
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+  
+def press_enter():
+    input("\nPress Enter to return to display_get_user_input.")
+
+class Roldan:
+    def __init__(self):
+        self.name = "Gian Rafael B. Roldan"
+        self.course = "BS Information Technology"
+        self.year = "2" 
+        self.section = "1"
+
+    def greet(self):
+        clear_screen()
+        show_border()
+        print("\t\t\t\t\tGREETINGS")
+        show_border()
+        print(
+            f"\nHi! My name is {self.name}.\n"
+            f"I am from {self.course} {self.year} - {self.section}.\n"
+        )
+        show_border()
+        press_enter()
+    
+    def show_takeaways(self):
+        clear_screen()
+        show_border()
+        print("\t\t\t\t\tMY TAKEAWAYS")
+        show_border()
+        print(
+            "This course taught me how to use Python, Git, and GitHub.\n",
+            "I learned that teamwork helps us grow faster.\n",
+            "Mistakes are normal and help me get better.\n",
+            "The best lesson is to keep learning and never give up.\n"
+        )
+        show_border()
+        press_enter()
+
+    def show_collaborative_experience(self):
+        clear_screen()
+        show_border()
+        print("\t\t\t\t\tCOLLABORATIVE EXPERIENCE")
+        show_border()
+        print(
+            "Working with my classmates helped me learn new things.\n",
+            "We solved problems together and supported each other.\n",
+            "Sharing ideas made our projects better.\n",
+            "Collaboration taught me the value of teamwork and communication.\n"
+        )
+        show_border()
+        press_enter()
+    
+    def show_skills(self):
+        clear_screen()
+        show_border()
+        print("\t\t\t\t\tSKILLS")
+        show_border()
+        print(
+            "I learned to use Git and GitHub for version control.\n",
+            "I improved my Python programming skills.\n",
+            "I learned to work better in a team.\n",
+            "I became more confident in my coding abilities.\n"
+        )
+        show_border()
+        press_enter()
+    
+    def show_about_integ(self):
+        clear_screen()
+        show_border()
+        print("\t\t\t\t\tABOUT INTE 202")
+        show_border()
+        print(
+            "Course: INTE 202: Integrative Programming and "
+            "Technologies 1"
+            ,"\nProfessor: Steven Villarosa"
+            ,"Schedule: Saturday | 7:30 - 12:30"
+        )
+        show_border()
+        press_enter()
+        
+    def show_menu(self):
+        user_input = 0
+        while user_input != 6:
+            user_input = self.display_get_user_input()
+            self.process_user_input(user_input)
+
+    def display_get_user_input(self):
+        while True:
+            clear_screen()
+            show_border()    
+            print("\t\t\t\t\tROLDAN MENU")
+            show_border()
+            print("\n1. Greetings")
+            print("2. My Takeaways") 
+            print("3. Collaborative Experience")
+            print("4. Skills")
+            print("5. About INTE 202") 
+            print("6. Return to Main Menu\n")
+            return int(input("Please select an option (1 - 6): "))
+            
+    def process_user_input(self, user_input):
+        match user_input:
+            case 1:
+                self.greet()
+            case 2:
+                self.show_takeaways()
+            case 3:
+                self.show_collaborative_experience()
+            case 4:
+                self.show_skills()
+            case 5:
+                self.show_about_integ()
+            case 6:
+                pass
+            case _:
+                input("\nWrong input. Press Enter to try again.")                    
+
+show = Roldan()
+show.show_menu()
